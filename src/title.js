fcfc1b96e1c1a2bffce7adf4837483ebb3f1b382
tavanhoi2000 <tavanhoi00@gmail.com>
2023-07-13 14:56:54 +0700

    update

--- src/title.js
+++ src/title.js
@@ -1,11 +1,12 @@
 function Title() {
-  return (
-    <div className="page-header">
-      <h1>
-        Project 01 - ToDo List <small>ReactJS</small>
-      </h1>
-    </div>
-  );
-}
-
-export default Title;
+    return (
+      <div className="page-header">
+        <h1>
+          Project 01 - ToDo List <small>ReactJS</small>
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Title;
+  
